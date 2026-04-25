5e4c2a6c91e3c345d07f0e8a54ef1959004f512f
arvinbehbahani562@gmail.com <arvinbehbahani562@gmail.com>
2026-04-26 00:56:58 +0330

    added swagger and redoc for post api

--- core/urls.py
+++ core/urls.py
@@ -20,6 +20,24 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="blog post API",
+        default_version='v1',
+        description="this is a Test api for post model",
+        contact=openapi.Contact(email="arvin@gmail.com"),
+        license=openapi.License(name="MIT License"),
+   ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 
 
 urlpatterns = [
@@ -28,6 +46,9 @@ urlpatterns = [
     path("api-auth/", include("rest_framework.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
     path('blog/', include("blog.urls")),
+    path("swagger/output.json", schema_view.without_ui(cache_timeout=0), name="schema-swagger-json"),
+    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc")
 ]
 
 
