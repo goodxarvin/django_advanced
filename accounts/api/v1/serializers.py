85d5769387f329a95b1503f83aea10cd7b1e796d
arvinbehbahani562@gmail.com <arvinbehbahani562@gmail.com>
2026-04-26 04:55:24 +0330

    added login and logout with token for account api

--- accounts/api/v1/serializers.py
+++ accounts/api/v1/serializers.py
@@ -1,7 +1,3 @@
-from typing import Any
-
-
-from unittest import expectedFailure
 from rest_framework import serializers
 from ...models import User
 from django.core import exceptions 
@@ -30,3 +26,47 @@ class RegistrationSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         validated_data.pop("password1", None)
         return User.objects.create_user(**validated_data)
+
+
+from django.contrib.auth import authenticate
+from django.utils.translation import gettext_lazy as _
+
+from rest_framework import serializers
+
+
+class CustomAuthTokenSerializer(serializers.Serializer):
+    email = serializers.CharField(
+        label=_("email"),
+        write_only=True
+    )
+    password = serializers.CharField(
+        label=_("Password"),
+        style={'input_type': 'password'},
+        trim_whitespace=False,
+        write_only=True
+    )
+    token = serializers.CharField(
+        label=_("Token"),
+        read_only=True
+    )
+
+    def validate(self, attrs):
+        username = attrs.get('email')
+        password = attrs.get('password')
+
+        if username and password:
+            user = authenticate(request=self.context.get('request'),
+                                username=username, password=password)
+
+            # The authenticate call simply returns None for is_active=False
+            # users. (Assuming the default ModelBackend authentication
+            # backend.)
+            if not user:
+                msg = _('Unable to log in with provided credentials.')
+                raise serializers.ValidationError(msg, code='authorization')
+        else:
+            msg = _('Must include "username" and "password".')
+            raise serializers.ValidationError(msg, code='authorization')
+
+        attrs['user'] = user
+        return attrs
--- accounts/api/v1/urls.py
+++ accounts/api/v1/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, include
 from . import views
 
+
 app_name = "api-v1"
 
 urlpatterns = [
     path("registration/", views.RgistrationAPIView.as_view(), name="account-register"),
+    path("login/token/", views.CustomAuthToken.as_view(), name="auth-token"),
+    path("logout/token/", views.CustomDiscardToken.as_view(), name="discard-token"),
+
 ]
