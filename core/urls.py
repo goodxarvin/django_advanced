e3b672aefe694ab075658256575fedee7d3bb999
arvinbehbahani562@gmail.com <arvinbehbahani562@gmail.com>
2026-05-09 21:48:47 +0330

    CD fixed 2

--- core/urls.py
+++ core/urls.py
@@ -37,7 +37,7 @@ schema_view = get_schema_view(
 )
 
 def IndexView(request):
-    return HttpResponse("<h1>home page (deploy works) and im main branch again -d </h1>")
+    return HttpResponse("<h1>home page (deploy works) and im main branch again -d -d</h1>")
 
 
 urlpatterns = [
